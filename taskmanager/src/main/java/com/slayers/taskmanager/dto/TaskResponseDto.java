3033f734c6d0315979ac16553b93cacc1bba7096
lalitchaudharry <lc7906798@gmail.com>
2026-02-09 11:57:53 +0530

    Task module CRUD implemented

---
+++ taskmanager/src/main/java/com/slayers/taskmanager/dto/TaskResponseDto.java
@@ -0,0 +1,18 @@
+package com.slayers.taskmanager.dto;
+
+import com.slayers.taskmanager.entity.TaskStatus;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+public class TaskResponseDto {
+
+    private Long id;
+    private String title;
+    private String description;
+    private TaskStatus status;
+    private LocalDateTime deadline;
+}
